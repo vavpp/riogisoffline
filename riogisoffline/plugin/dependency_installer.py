8868f0cadcc32e0f61b2678f69c1cbc6ca9cd086
Paal Pedersen <paal.pedersen@vav.oslo.kommune.no>
2024-04-15 10:08:19 +0200

    oppdatert hvor azure installeres

--- riogisoffline/plugin/dependency_installer.py
+++ riogisoffline/plugin/dependency_installer.py
@@ -7,14 +7,8 @@ import riogisoffline.plugin.utils as utils
 class DependencyInstaller:
     def __init__(self):
         self.exe = sys.executable.replace('qgis-bin', 'python')
-
-        self.target = utils.get_plugin_dir("dep")
         self.requirements_path = utils.get_plugin_dir("requirements.txt")
-
-        if not os.path.exists(self.target):
-            os.makedirs(self.target, exist_ok=True)
-        sys.path = [self.target] + sys.path
         
     def install_requirements(self):
-        cmd = f"{self.exe} -m pip install -r {self.requirements_path} -t {self.target}"
+        cmd = f"{self.exe} -m pip install -r {self.requirements_path} --user"
         subprocess.call(cmd)
