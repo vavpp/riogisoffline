3d56e49a80e1f431a940b66e7a0964dadb4c1ad5
Olav Nybø <165135412+olavny-vav@users.noreply.github.com>
2024-05-27 14:58:52 +0200

    Add util tests

--- test_riogisoffline/test_utils.py
+++ test_riogisoffline/test_utils.py
@@ -2,12 +2,24 @@ import pytest
 
 from riogisoffline.plugin.utils import (
     get_plugin_dir,
-    load_json
+    load_json,
+    get_settings_path,
+    get_user_settings_path
 )
 
 def test_get_plugin_dir():
     assert get_plugin_dir()
 
+def test_get_settings_path():
+    assert get_settings_path()
+    assert "riogisoffline" in get_settings_path()
+    assert "settings.json" in get_settings_path()
+
+def test_get_user_settings_path():
+    assert get_user_settings_path()
+    assert "riogisoffline" in get_user_settings_path()
+    assert "bruker_settings.json" in get_user_settings_path()
+
 def test_load_json():
-    path = get_plugin_dir("test")
+    path = get_settings_path()
     assert load_json(path)
